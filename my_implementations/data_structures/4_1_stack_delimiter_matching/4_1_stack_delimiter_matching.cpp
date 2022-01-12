6ba76164a39649cfd1fbc49dfc266d6052aacf05
dim web <dmitrymail_k@mail.ru>
2022-01-12 16:15:57 +0300

    read file c++

---
+++ my_implementations/data_structures/4_1_stack_delimiter_matching/4_1_stack_delimiter_matching.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+#include "../4__stack/stack.cpp"
+
+string read_file(string filename)
+{
+  // solution from https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+  ifstream file(filename);
+  string file_content;
+  stringstream file_stream;
+  if (file.is_open())
+  {
+    file_stream << file.rdbuf();
+    file_content = file_stream.str();
+    file.close();
+  }
+  else
+  {
+    file_content = "";
+  }
+
+  return file_content;
+}
+
+int main()
+{
+  string file_content = read_file("./test_cpp_file");
+  cout << file_content << "\n";
+}
