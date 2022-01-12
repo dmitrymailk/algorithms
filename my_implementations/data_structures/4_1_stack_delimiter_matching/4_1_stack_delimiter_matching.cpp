9517de40f87d97f2a4b93475845701b9892a8c84
dim web <dmitrymail_k@mail.ru>
2022-01-12 21:24:14 +0300

    create delimeter matcher

--- my_implementations/data_structures/4_1_stack_delimiter_matching/4_1_stack_delimiter_matching.cpp
+++ my_implementations/data_structures/4_1_stack_delimiter_matching/4_1_stack_delimiter_matching.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <map>
 using namespace std;
 
 #include "../4__stack/stack.cpp"
@@ -30,53 +31,73 @@ string read_file(string filename)
 void delimeter_matcher(string file_content)
 {
   Stack<char> *open_brackets = new Stack<char>();
+  bool status = true;
 
-  // pair <char, bool, bool>
+  map<char, int> parenthesis = {
+      {'(', 0},
+      {')', 0},
+      {'[', 1},
+      {']', 1},
+      {'{', 2},
+      {'}', 2},
+  };
 
-  auto it = file_content.begin();
-  while (it != file_content.end())
+  int i = 0;
+  while (i < file_content.length())
   {
-    char temp_char = *it;
+    char temp_char = file_content[i];
     if (temp_char == '(' || temp_char == '[' || temp_char == '{')
     {
       open_brackets->Push(temp_char);
-      it += 1;
     }
     else if (temp_char == ')' || temp_char == ']' || temp_char == '}')
     {
       char open_bracket = open_brackets->Pop();
-      if (open_bracket != temp_char)
+      if (parenthesis[open_bracket] != parenthesis[temp_char])
       {
         cout << "Error, expected" << open_bracket << " Got: " << temp_char << "\n";
+        status = false;
+        break;
       }
-      break;
     }
     else if (temp_char == '/')
     {
-      char next_char = *(it + 1);
+      char next_char = file_content[i + 1];
       if (next_char == '*')
       {
+        i += 1;
         // пока не будет найдена строка */
         // или не достигнут конец файла выполнять поиск
-        while (*it != '*' && *(it + 1) != '/' || it != file_content.end())
+        char test, test2;
+        while (((int)(file_content[i] == '*') + (int)(file_content[i + 1] == '/')) != 2 &&
+               i < file_content.length())
         {
-          it += 1;
+          test = file_content[i];
+          test2 = file_content[i + 1];
+          i += 1;
         }
-        if (it == file_content.end())
+        if (i >= file_content.length())
         {
           cout << "Error: comment block isn't closed"
                << "\n";
+          status = false;
           break;
         }
-      }
-      else
-      {
-        it += 1;
+        i += 1;
+        // continue;
       }
     }
-    else
-      it += 1;
+    i += 1;
   }
+
+  if (status)
+
+    cout << "Everything ok"
+         << "\n";
+
+  else
+    cout << "Parenthesis don't match"
+         << "\n";
 }
 
 int main()
